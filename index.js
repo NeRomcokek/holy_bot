895fe8f03d28ef80462b6ed2c808123e512b0f45
Roman <romanberezitskyi@gmail.com>
2026-06-17 23:59:44 +0300

    Виправив index.js

--- index.js
+++ index.js
@@ -6,17 +6,11 @@ const state = {
     isCaptchaWaiting: false,
     isSneaking: false,
     chatLog: [],
-    currentWindow: null,
-    serverStarted: false
+    currentWindow: null
 };
 
 // 1. Ініціалізуємо бота
 const bot = initBot(state);
 
-// 2. Запускаємо веб-сервер при першому спавні
-bot.once('spawn', () => {
-    if (!state.serverStarted) {
-        startWebServer(3000, bot, state);
-        state.serverStarted = true;
-    }
-});
+// 2. Запускаємо веб-сервер ВІДРАЗУ, не чекаючи спавну бота
+startWebServer(3000, bot, state);
