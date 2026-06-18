7f7def0a870f7bb0253d1cb569c4589e58b3ea18
Roman <romanberezitskyi@gmail.com>
2026-06-18 17:34:08 +0300

    .

--- checker.js
+++ checker.js
@@ -6,7 +6,7 @@ const { createCanvas } = require('canvas');
 const { getItemName } = require('./src/utils/textParser');
 
 // ================= НАЛАШТУВАННЯ =================
-const PASSWORD = 'ТВІЙ_ПАРОЛЬ';
+const PASSWORD = 'tony0905stark';
 const START_ACCOUNT = 1;
 const END_ACCOUNT = 200;
 
