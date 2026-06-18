d4f4d1af5d77f8a2f0c7862a0e52b56119012f7e
Roman <romanberezitskyi@gmail.com>
2026-06-18 23:34:03 +0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { initBot } = require('./src/bot/botCore');
 const { startWebServer } = require('./src/web/server');
 
-// Читаємо аргументи запуску (process.argv[2] - це перше слово після node index.js)
-// Якщо нічого не передали, ставимо стандартні значення
 const botName = process.argv[2] || 'Romcokek1';
 const webPort = parseInt(process.argv[3]) || 3000;
 
@@ -10,20 +8,12 @@ const state = {
     isCaptchaWaiting: false,
     isSneaking: false,
     chatLog: [],
-    currentWindow: null,
-    serverStarted: false
+    currentWindow: null
 };
 
 console.log(`\n🤖 Ініціалізація бота: ${botName}`);
-console.log(`🌐 Панель керування буде на порту: ${webPort}\n`);
 
-// Передаємо ім'я в ядро бота
 const bot = initBot(state, botName);
 
-bot.once('spawn', () => {
-    if (!state.serverStarted) {
-        // Передаємо порт у веб-сервер
-        startWebServer(webPort, bot, state);
-        state.serverStarted = true;
-    }
-});
+// Запускаємо веб-сервер ОДРАЗУ, щоб панель була доступна 24/7
+startWebServer(webPort, bot, state);
